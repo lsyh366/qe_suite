00810166aa0b2221afa59e13a85cc00b1117fbe9
edmontoneuler <cameron.rudderham@gmail.com>
2019-01-25 17:01:49 -0400

    updated bash maker, removed unecessary stencil

--- code/scf/bash_makers.py
+++ code/scf/bash_makers.py
@@ -1,27 +1,29 @@
-""" make_bash(): Creates a Quantum Espresso input file for a self-consistent calculation
-
-"""
 import os
 import sys
 
-def make_bash_niagara(filename, bash_stencil = 'bash_stencil', nodes=1, ntasks=40, time='04:00:00', mail_type = 'END,FAIL', mail_user = 'slurmnotifs@gmail.com', account ='rrg-maassenj', job_name = 'scf', executable = 'pw.x', qe_inputfile = 'scf.in', qe_outputfile = 'scf.out'):
-
-    s = open(bash_stencil).read()
-    s = s.replace('NODES', str(nodes))
-    s = s.replace('NTASKS', str(ntasks))
-    s = s.replace('TIME', time)
-    s = s.replace('MAIL-TYPE', mail_type)
-    s = s.replace('MAIL-USER', mail_user)
-    s = s.replace('ACCOUNT', account)
-    s = s.replace('JOB-NAME', job_name)
-
-    s = s.replace('EXECUTABLE', executable)
-    s = s.replace('QE_INPUTFILE', qe_inputfile)
-    s = s.replace('QE_OUTPUTFILE', qe_outputfile)
-
+def make_bash_niagara(filename, nodes=1, ntasks=40, time='04:00:00', mail_type = 'END,FAIL', mail_user = 'slurmnotifs@gmail.com', account ='rrg-maassenj', job_name = 'scf', executable = 'pw.x', qe_inputfile = 'scf.in', qe_outputfile = 'scf.out'):
 
     f = open(filename, 'w')
-    f.write(s)
+    f.write('#!/bin/bash\n')
+    f.write('#SBATCH --nodes='+str(nodes)+'\n')
+    f.write('#SBATCH --ntasks='+str(ntasks)+'\n')
+    f.write('#SBATCH --time=' + time +'\n')
+    f.write('#SBATCH --mail-type=' + mail_type+'\n')
+    f.write('#SBATCH --mail-user=' + mail_user+'\n')
+    f.write('#SBATCH --account=' + account+'\n')
+    f.write('#SBATCH --job-name=' + job_name+'\n')
+    f.write('\n')
+
+    f.write('module load CCEnv \n')
+    f.write('module load StdEnv \n')
+    f.write('module load nixpkgs/16.09 \n')
+    f.write('module load intel/2016.4 \n')
+    f.write('module load openmpi/2.1.1 \n')
+    f.write('module load quantumespresso/6.1 \n')
+    f.write('\n')
+
+    f.write('srun ' + executable + ' < ' + qe_inputfile + ' > ' + qe_outputfile +'\n')
+    
     f.close()
     
     os.system('dos2unix ' + filename)
