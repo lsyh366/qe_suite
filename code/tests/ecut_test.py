4dee491443450fb4ee14fb5f59075bdec846f15d
Cameron Rudderham <Cameron.Rudderham@gmail.com>
2019-01-27 20:22:24 -0400

    commented ecut_test.py

--- code/tests/ecut_test.py
+++ code/tests/ecut_test.py
@@ -7,20 +7,20 @@ from kpts_makers import make_auto_kpts
 from param_makers import make_scf_param
 from bash_makers import make_bash_niagara 
 
-values = [50,55,60]
-make_auto_kpts('KPTS', grid = [11,11,1,0,0,0])
-make_Bi2Te3_QL('STRUCT')
+values = [50,55,60]                            # Values of ecutwfc to use
+make_auto_kpts('KPTS', grid = [11,11,1,0,0,0]) # KPTS for this calculation
+make_Bi2Te3_QL('STRUCT')                       # STRUCT file for this calculation
 N = len(values)
 
 for k in range(N):
     iteration = 'ecutwfc' + str(values[k])
     os.system('mkdir ' + iteration)
-    make_scf_param('PARAMS', iteration, pseudo_dir = '../../PP/', ecutwfc = values[k], ecutrho = 4*values[k])
+    make_scf_param('PARAMS', iteration, pseudo_dir = '../../PP/', ecutwfc = values[k], ecutrho = 4*values[k]) #non-default scf parameters
     os.system('cat PARAMS STRUCT KPTS >> scf.in')
     os.system('dos2unix scf.in')
     os.system('mv scf.in  ' + iteration)
 
-    make_bash_niagara('scf.sh', job_name = 'Bi2Te3_QL' +'_' + iteration)
+    make_bash_niagara('scf.sh', job_name = 'Bi2Te3_QL' +'_' + iteration) #Job name
     os.system('mv scf.sh ' + iteration)
 
     os.chdir(iteration)
