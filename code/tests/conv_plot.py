a71e434214aba7341b6de66dec58d2dd516449e2
edmontoneuler <cameron.rudderham@gmail.com>
2019-01-31 20:59:58 -0400

    improved conv_plot

--- code/tests/conv_plot.py
+++ code/tests/conv_plot.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+Ry2eV = 13.60569 #Conversion from Rydberg to eV
 data = np.loadtxt('conv_plot_data')
 
 ecuts  = np.array(data[:, 0]) 
 energy = data[:, 1]
-delta_energy = np.array([abs(E - energy[-1]) for E in energy])
+delta_energy = Ry2eV*np.array([abs(E - energy[-1]) for E in energy])
 energy = np.array(energy)
 
-plt.plot(ecuts, np.log(delta_energy))
-plt.xlabel('Energy cutoff (Ry)')
-plt.ylabel('Energy difference (log scale)')
+plt.plot(Ry2eV*ecuts, np.log(delta_energy))
+plt.axhline(y=-3, color='b') # 1 meV convergence
+plt.xlabel('Energy cutoff (eV)')
+plt.ylabel('Energy difference (log(eV))')
 plt.title('Kinetic Energy Cutoff Convergence')
 plt.show()
 
