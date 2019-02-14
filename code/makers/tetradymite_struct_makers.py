4c44e548695bbd42bbd1a05211984bf2ff0ee8b8
edmontoneuler <cameron.rudderham@gmail.com>
2019-02-14 17:16:37 -0400

    updated tetradymite structure makers

--- code/makers/tetradymite_struct_makers.py
+++ code/makers/tetradymite_struct_makers.py
@@ -9,10 +9,6 @@ def make_bulk_Bi2Te3_struc(struc_filename, a = 4.383, c = 30.487, mu = 0.212000,
     v3 = [0, a/np.sqrt(3), c/3]
     
     f = open(struc_filename, 'w+')
-
-    f.write('    ibrav = 0,\n')
-    f.write('    nat = 5, ntyp = 2\n')
-    f.write('/\n')
     
     f.write('ATOMIC_SPECIES\n')
     f.write('Bi 208.9804 Bi.rel-pbe-dn-kjpaw_psl.1.0.0.UPF\n')
@@ -43,10 +39,6 @@ def make_bulk_Bi2Se3_struc(struc_filename, a = 4.138, c = 28.64, mu = 0.211000, 
     
     f = open(struc_filename, 'w+')
 
-    f.write('    ibrav = 0,\n')
-    f.write('    nat = 5, ntyp = 2\n')
-    f.write('/\n')
-    
     f.write('ATOMIC_SPECIES\n')
     f.write('Bi 208.9804 Bi.rel-pbe-dn-kjpaw_psl.1.0.0.UPF\n')
     f.write('Se 78.9600  Se.rel-pbe-dn-kjpaw_psl.1.0.0.UPF\n')
@@ -75,10 +67,6 @@ def make_bulk_Sb2Te3_struc(struc_filename, a = 4.250, c = 30.35, mu = 0.212000, 
     v3 = [0, a/np.sqrt(3), c/3]
     
     f = open(struc_filename, 'w+')
-
-    f.write('    ibrav = 0,\n')
-    f.write('    nat = 5, ntyp = 2\n')
-    f.write('/\n')
     
     f.write('ATOMIC_SPECIES\n')
     f.write('Sb 121.760 Bi.rel-pbe-dn-kjpaw_psl.1.0.0.UPF\n')
